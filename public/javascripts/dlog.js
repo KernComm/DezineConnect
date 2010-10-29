373cfa480f5f798352438f73b1f4530c6b51de9a
Rajeshwar Mothe <rajeshwar@kern-comm.com>
2010-10-29 16:43:53 +0530

    Dlog home page changes

--- public/javascripts/dlog.js
+++ public/javascripts/dlog.js
@@ -5,14 +5,14 @@
 
 				//Caption Sliding (Partially Hidden to Visible)
 				$('.boxgrid.caption').hover(function(){
-					$(".cover", this).stop().animate({top:'90px'},{queue:false,duration:500});
+					$(".cover", this).stop().animate({top:'65px'},{queue:false,duration:500});
 				}, function() {
 					$(".cover", this).stop().animate({top:'120px'},{queue:false,duration:500});
 				});
 
                                 $('.boxgrid_big.caption_big').hover(function(){
 
-					$(".cover_big", this).stop().animate({top:'195px'},{queue:false,duration:500});
+					$(".cover_big", this).stop().animate({top:'180px'},{queue:false,duration:500});
 				}, function() {
 
 					$(".cover_big", this).stop().animate({top:'240px'},{queue:false,duration:500});
